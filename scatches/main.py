d2a1fa27da7f2ac1e0cd6d35a799bcd4c6906761
Lo-Lap <lapina.ok@edu.spbstu.ru>
2025-10-08 15:48:14 +0300

    add new user routes

--- scatches/main.py
+++ scatches/main.py
@@ -25,12 +25,43 @@ def main():
     # player = steam.get_player("76561197960435530")
     # print(f"Игрок: {player.get('personaname')}")
     #
+    # players_info = steam.get_players_info(["76561197960435530", "76561197960265731"])
+    # if players_info:
+    #     for info in players_info:
+    #         print(f"Игрок: {info.get('personaname')}")
+    #         communityvisibility = info.get('communityvisibilitystate')
+    #         visibility_str = ""
+    #         match communityvisibility:
+    #             case 1:
+    #                 visibility_str = "Приватный"
+    #             case 2:
+    #                 visibility_str = "Открыт только друзьям"
+    #             case 3:
+    #                 visibility_str = "Публичный"
+    #         print(f"Видимость профиля Steam: {visibility_str}")
+
+    # Получаем достижения игрока из его списков игр
+    # player_id = "76561197960265731"
+    # game_ids = ["10", "730", "646570", "632360"]
+    # for game_id in game_ids:
+    #     achievements = steam.get_player_achievements_by_one_game(player_id, game_id)
+    #     print(f"appid = {game_id}")
+    #     if achievements:
+    #         print(f"count achievements = {len(achievements)}")
+    #     stats = steam.get_player_game_stats(player_id, game_id)
+    #     if stats:
+    #         print(f"count all stats = {len(stats)}")
+    #         print(f"count stats with value > 0 = "
+    #               f"{len([stat for stat in stats if stat.get('value') > 0])}\n")
+    #
     # # Полный анализ игрока
     # profile = steam.get_player_full_profile("76561197960435530")
+    # print(f"Уровень Steam игрока: {profile['steam_level']}")
     # print(f"Игр в библиотеке: {profile['games_count']}")
     # print(f"Всего друзей: {profile['friends_count']}")
-    #
-    # # Анализ предпочтений
+    # print(f"Список steam id друзей: {profile['friends_ids']}")
+
+    # Анализ предпочтений
     # analysis = steam.analyze_player("76561197960287966")
     # print(f"Общее время в играх: {analysis['total_playtime_hours']} часов")
 
@@ -43,7 +74,6 @@ def main():
     # mediator.create_game()
     # game_analysis = steam.analyze_game(730)
     # print(f"Положительных отзывов: {game_analysis['review_analysis']['positive_rate']:.1%}")
-
 
 
 if __name__ == "__main__":
